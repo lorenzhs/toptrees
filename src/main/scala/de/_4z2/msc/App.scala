7cd58ba18edc68b41acc9868cd97639c4fd40de1
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-06-23 15:08:22 +0100

    [code] implement merging of nodes in a tree

--- src/main/scala/de/_4z2/msc/App.scala
+++ src/main/scala/de/_4z2/msc/App.scala
@@ -1,11 +1,27 @@
 package de._4z2.msc
 
-/**
- */
+import scala.collection.mutable.ArrayBuffer
+
 object App {
 
   def main(args : Array[String]) {
-    println( "Hello World!" )
+    val root = new Node[Int](0)
+    val tree = new Tree[Node[Int]](root)
+    (1 to 3).foreach(i => root.newChild(i))
+    (1 to 2).foreach(i => root.children(0).newChild(i+3))
+    root.children(2).newChild(6)
+
+    println(tree)
+    root.children(0).children(1).mergeWithLeftSibling
+    println(tree)
+    root.children(0).mergeWithRightSibling
+    println(tree)
+    root.children(1).mergeWithOnlyChild
+    println(root)
+    root.children(0).mergeWithRightSibling
+    println(root)
+    root.children(0).mergeWithOnlyChild
+    println(root)
   }
 
 }
--- src/main/scala/de/_4z2/msc/Tree.scala
+++ src/main/scala/de/_4z2/msc/Tree.scala
@@ -26,23 +26,64 @@ class BinNode[DataType](val data: DataType, val parent: BinNode[DataType]) {
     "(" + List(left, Some(data), right).map(_.getOrElse("_").toString).mkString(",") + ")"
 }
 
-class Node(val parent: Node) {
-  var index = -1
-  val children = new ArrayBuffer[Node]()
-  def this() { this(null) }
+class Node[DataType](var parent: Node[DataType]) {
+  var data: Option[DataType] = None
+  var index:Int = -1
+  var mergeType:Int = -1
+  var children = new ArrayBuffer[Node[DataType]]()
+  def this(_data: DataType) { this(null); data = Some(_data) }
+  def this(parent: Node[DataType], _data: DataType) { this(parent); data = Some(_data) }
 
-  def addChild(child: Node) { child.index = numChildren; children += child }
+  def addChild(child: Node[DataType]):Node[DataType] = { child.index = numChildren; children += child; child }
+  def newChild(data: DataType):Node[DataType] = addChild(new Node[DataType](this, data))
+  def newChild():Node[DataType] = addChild(new Node[DataType](this))
+  def setChild(index: Int, child: Node[DataType]) = { child.index = index; children(index) = child; child }
+  def removeChild(index: Int) { children.remove(index); children.foreach(child => if (child.index > index) child.index -= 1) }
   def isLeaf = children.isEmpty
+  def isInnerNode = parent != null && !children.isEmpty
+  def isRoot = parent == null;
   def numChildren = children.size
   def hasSiblings = parent match {
     case null => false
     case node => node.children.size > 1
   }
-  def isFirstChild = index == 0
-  def isLastChild = index == parent.numChildren-1
+  def isFirstChild = { assert(!isRoot); index == 0 }
+  def isLastChild = { assert(!isRoot || { println(toString()); false }); index == parent.numChildren-1 }
   def leftSibling = if (isFirstChild) None else Some(parent.children(index - 1))
   def rightSibling = if (isLastChild) None else Some(parent.children(index + 1))
 
-  override def toString = if (isLeaf) "_" else
-    "(" + children.map(_.toString).mkString(",") + ")"
+  def mergeWithLeftSibling = { assert(!isFirstChild); mergeWithSibling(index-1) }
+  def mergeWithRightSibling = { assert(!isLastChild); mergeWithSibling(index+1) }
+  private def mergeWithSibling(otherIndex: Int) = {
+    assert(Math.abs(index-otherIndex) == 1)
+    val left  = parent.children(Math.min(index, otherIndex))
+    var right = parent.children(Math.max(index, otherIndex))
+
+    val newNode = new Node[DataType](parent)
+    newNode.mergeType = (left.isLeaf, right.isLeaf) match {
+      case (true, true)  => 4
+      case (true, false) => { newNode.children = right.children; 3 }
+      case (false, true) => { newNode.children = left.children; 2 }
+      case _ => { assert(false); -1 }  // not allowed
+    }
+    parent.setChild(Math.min(index, otherIndex), newNode)
+    parent.removeChild(Math.max(index, otherIndex))
+    newNode  // return new node
+  }
+
+  def mergeWithOnlyChild = {
+    assert(numChildren == 1)
+    val child = children(0)
+    if (child.isLeaf) {
+      mergeType = 1
+      children.clear()
+    } else {
+      child.mergeType = 0
+      child.parent = parent
+      parent.setChild(index, child);
+    }
+  }
+
+  override def toString = if (isLeaf) data.getOrElse("_").toString else
+    "(" + data.getOrElse("_").toString + "/" + children.map(_.toString).mkString(",") + ")"
 }
