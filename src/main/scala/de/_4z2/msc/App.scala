129d9c10e3a96964b2aab202d631d8b047f250a0
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-06-24 14:24:12 +0100

    Ordered tree sample

--- src/main/scala/de/_4z2/msc/App.scala
+++ src/main/scala/de/_4z2/msc/App.scala
@@ -22,6 +22,18 @@ object App {
     println(root)
     root.children(0).mergeWithOnlyChild(11)
     println(root)
+    println()
+
+    val t = new OrderedTree[TreeNode, TreeEdge](() => new TreeNode(), () => new TreeEdge())
+    t.addNodes(7)
+    List(0->1, 0->2, 0->3, 1->4, 1->5, 3->6).foreach(p => t.addEdge(p._1, p._2))
+    println(t)
+    t.mergeNodeWithSibling(4,5)
+    t.mergeNodeWithSibling(1,2)
+    t.mergeNodeWithOnlyChild(3)
+    t.mergeNodeWithSibling(1,3);
+    t.mergeNodeWithOnlyChild(1);
+    println(t)
   }
 
 }
