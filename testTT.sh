6c43637ef0378626163395b9c3de59dcef61af53
Lorenz Hübschle-Schneider <lorenz-dev@lgh-alumni.de>
2014-07-08 11:19:27 +0100

    test scripts

--- testTT.sh
+++ testTT.sh
@@ -2,6 +2,8 @@
 OUT=ext/out
 for file in ext/{,forlorenz/}*.xml; do
 	echo "Testing with file $file at `date`"
-	./test $file
+	./testTT $file $OUT
+	md5sum $OUT/{orig,unpacked,toptree,uncomp_toptree}.xml
+	rm -f $OUT/*.xml
 	echo ""
 done | tee $1
