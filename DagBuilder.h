lorenzhs/toptrees

commit a89de19dec732bfd04d184f8ff2253b5d95e99c8

Remove the need for the pseudo-cluster in the top tree

DagBuilder.h
@@ -50,6 +50,8 @@ public:
 		: topTree(t), dag(dag), nodeMap(), clusterToDag(t.clusters.size(), -1) {}
 
 	void createDag() {
+		// need to add the root separately, it isn't referenced by an edge in the top tree
+		dag.nodes[addClusterToDag(0)].label = topTree.clusters[0].label;
 		topTree.inPostOrder([&](const int clusterId) { addClusterToDag(clusterId); });
 	}
 
@@ -103,7 +105,7 @@ private:
 template <typename DataType>
 class BinaryDagUnpacker {
 public:
-	BinaryDagUnpacker(BinaryDag<DataType> &dag, TopTree<DataType> &topTree) : nextLeafId(0), dag(dag), topTree(topTree) {}
+	BinaryDagUnpacker(BinaryDag<DataType> &dag, TopTree<DataType> &topTree) : nextLeafId(1), dag(dag), topTree(topTree) {}
 
 	void unpack() {
 		assert((int)topTree.clusters.size() == topTree.numLeaves);
@@ -111,6 +113,8 @@ public:
 			[&](const int nodeId, const int left, const int right) {
 				return addNodeToTopTree(nodeId, left, right);
 			});
+		// Restore the root label
+		topTree.clusters[0].label = dag.nodes[1].label;
 	}
 
 private:

TopTreeConstructor.h
@@ -102,7 +102,6 @@ protected:
 			iteration++;
 			tree.checkConsistency();
 		}
-		mergeCallback(0, tree.edges[tree.nodes[0].firstEdgeIndex].headNode, 0, VERT_WITH_BBN);
 		// reset the output stream
 		cout.unsetf(std::ios_base::fixed);
 		cout << std::setprecision(precision);

Entropy.h
@@ -162,8 +162,9 @@ struct DagEntropy {
 			}
 		});
 
-		// nodeId starts at 1 because 0 is a dummy node, we don't need to code it
-		for (uint nodeId = 1; nodeId < dag.nodes.size(); ++nodeId) {
+		// nodeId starts at 2 because 0 is a dummy node, we don't need to code it,
+		// and 1 is the tree's root, but we know that, so we don't need to code it.
+		for (uint nodeId = 2; nodeId < dag.nodes.size(); ++nodeId) {
 			// DAG node is coded as the IDs of its children, its own ID
 			// is implicit from the position in the output it appears in
 

TopTree.h
@@ -107,7 +107,7 @@ struct TopTree {
 	int height() const {
 		return foldPostOrder<int>([](const int leftHeight, const int rightHeight) {
 			return std::max(leftHeight, rightHeight) + 1;
-		}, -1);
+		}, 0);
 	}
 
 	double avgDepth() const {
@@ -197,15 +197,13 @@ public:
 	void unpack() {
 		int firstId = tree.addNodes(topTree.numLeaves);
 		assert(firstId == 0);
-		int rootClusterId = topTree.clusters.size() - 1;
 
-		Cluster<DataType> &rootCluster = topTree.clusters[rootClusterId];
 		// special treatment for root node of original tree
-		const DataType *label = topTree.clusters[rootCluster.left].label;
+		const DataType *label = topTree.clusters[0].label;
 		assert(label != NULL);
 		labels.set(0, *label);
 		// unpack the rest
-		unpackCluster(rootCluster.right, firstId);
+		unpackCluster(topTree.clusters.size() - 1, firstId);
 	}
 
 private:

commit fcea36201fd8daa67ac30365fb4f789017436223

Fix a really stupid bug introduced in a89de19
Basically, revert the bad parts

DagBuilder.h
@@ -98,7 +98,7 @@ private:
 template <typename DataType>
 class BinaryDagUnpacker {
 public:
-	BinaryDagUnpacker(BinaryDag<DataType> &dag, TopTree<DataType> &topTree) : nextLeafId(1), dag(dag), topTree(topTree) {}
+	BinaryDagUnpacker(BinaryDag<DataType> &dag, TopTree<DataType> &topTree) : nextLeafId(0), dag(dag), topTree(topTree) {}
 
 	void unpack() {
 		assert((int)topTree.clusters.size() == topTree.numLeaves);

TopTreeConstructor.h
@@ -99,6 +99,7 @@ protected:
 			iteration++;
 			tree.checkConsistency();
 		}
+		mergeCallback(0, tree.edges[tree.nodes[0].firstEdgeIndex].headNode, 0, VERT_WITH_BBN);
 		// reset the output stream
 		cout.unsetf(std::ios_base::fixed);
 		cout << std::setprecision(precision);

TopTreeUnpacker.h
@@ -68,7 +68,12 @@ private:
 		const Cluster<DataType> &cluster = topTree.clusters[clusterId];
 		int boundaryNode(nodeId);
 		if (isLeaf(cluster.left)) {
-			tree.addEdge(nodeId, cluster.left, extraSpace);
+			if (cluster.left != 0) {
+				tree.addEdge(nodeId, cluster.left, extraSpace);
+			} else {
+				// Don't add the loop
+				assert(nodeId == 0);
+			}
 			handleLeaf(cluster.left);
 			boundaryNode = cluster.left;
 		} else {