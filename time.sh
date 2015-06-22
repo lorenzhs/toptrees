e503f631a315db593bb742e4104aea93d503e242
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-06-22 13:40:07 +0200

    Rerun experiments, experiment script

--- time.sh
+++ time.sh
@@ -17,18 +17,22 @@ run() {
 
 rm -f $LOG $LOG_C $LOG_R
 
-# Run on stripped files
-for f in xml/*.xml; do
-	echo -n "Reading $f... " | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
-	fn=/tmp/ramdisk/$(echo $f | sed 's,.*/,,');
-	cp $f $fn; # read file to RAM
-	echo "done." | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
-	run "gzip" "-fk9" "gzip9" $fn $f          | tee -a $LOG
-	run "bzip2" "-fk" "bzip2" $fn $f          | tee -a $LOG
-	run "./repair-p" "" "repair" $fn $f       | tee -a $LOG
-	run "./coding-p" "" "tt-classic" $fn $f   | tee -a $LOG_C
-	run "./coding-p" "-r" "tt-repair" $fn $f  | tee -a $LOG_R
-	run "./TreeRePair" "" "treerepair" $fn $f | tee -a $LOG
-	rm $fn;
-	echo "" | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
+# Run up to 30 iterations
+# (one iteration takes ~20min)
+for it in {0..29}; do
+	# Run on stripped files
+	for f in /data01/xml/xml/*.xml; do
+		echo -n "Reading $f... " | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
+		fn=/tmp/ramdisk/$(echo $f | sed 's,.*/,,');
+		cp $f $fn; # read file to RAM
+		echo "done." | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
+		run "gzip" "-fk9" "gzip9" $fn $f          | tee -a $LOG
+		run "bzip2" "-fk" "bzip2" $fn $f          | tee -a $LOG
+		run "./repair-p" "" "repair" $fn $f       | tee -a $LOG
+		run "./coding-p" "" "tt-classic" $fn $f   | tee -a $LOG_C
+		run "./coding-p" "-r" "tt-repair" $fn $f  | tee -a $LOG_R
+		run "./TreeRePair" "" "treerepair" $fn $f | tee -a $LOG
+		rm $fn;
+		echo "" | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
+	done
 done
