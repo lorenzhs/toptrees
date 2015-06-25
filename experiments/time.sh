8941998176416bf48ffadc893f89adfa32e2501e
Lorenz Hübschle-Schneider <lorenzhs@ira.uka.de>
2015-06-25 11:07:17 +0200

    Rerun experiments

--- experiments/time.sh
+++ experiments/time.sh
@@ -18,12 +18,12 @@ run() {
 rm -f $LOG $LOG_C $LOG_R
 
 # Run up to 30 iterations
-# (one iteration takes ~20min)
+# (one iteration of top tree compression takes ~20min)
 for it in {0..29}; do
 	# Run on stripped files
 	for f in /data01/xml/xml/*.xml; do
 		echo -n "Reading $f... " | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
-		fn=/tmp/ramdisk/$(echo $f | sed 's,.*/,,');
+		fn=/tmp/$(echo $f | sed 's,.*/,,');
 		cp $f $fn; # read file to RAM
 		echo "done." | tee -a $LOG_C | tee -a $LOG_R | tee -a $LOG
 		run "gzip" "-fk9" "gzip9" $fn $f          | tee -a $LOG
